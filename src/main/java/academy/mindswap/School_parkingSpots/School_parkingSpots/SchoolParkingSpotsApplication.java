2eafa12cd74633f326c63215eca8becb3c448223
David Mendes <david.mendes@mindswap.academy>
2022-09-06 11:26:06 +0100

    creating git

---
+++ src/main/java/academy/mindswap/School_parkingSpots/School_parkingSpots/SchoolParkingSpotsApplication.java
@@ -0,0 +1,13 @@
+package academy.mindswap.School_parkingSpots.School_parkingSpots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolParkingSpotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolParkingSpotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/mindswap/School_parkingSpots/School_parkingSpots/SchoolParkingSpotsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mindswap.School_parkingSpots.School_parkingSpots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolParkingSpotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
